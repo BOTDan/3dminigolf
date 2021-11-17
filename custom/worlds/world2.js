68d45a8ee8d3824967316ea2f22ca68be5121d31
Dan Wells <dantheradarman@gmail.com>
2021-11-17 23:34:40 +0000

    Redid altitude models

--- custom/worlds/world2.js
+++ custom/worlds/world2.js
@@ -8,15 +8,28 @@ class World2 extends World {
 
     this.ball.radius = 0.025;
 
-    const level = ModelCache.newModel("MINIGOLF_Altitude_Hole10");
-    level.position = new Vector(0, -1.667, 8.5)
-    level.calcColour = Model.flatLighting;
-    // level.texture = assets["snake_texture_2.tex"];
-    this.addModel(level);
 
-    const levelPhys = ModelCache.newModel("MINIGOLF_Altitude_Hole10");
-    levelPhys.position = level.position;
-    this.addPhysicsModel(levelPhys);
+    const ground = ModelCache.newModel("TOWERUNITE_Altitude_10_Ground");
+    ground.position = new Vector(0, -1.667, 8.5);
+    ground.rotation = new Angle(0, -90, 0);
+    ground.scale = new Vector(0.325);
+    ground.calcColour = Model.flatColourLighting(util.RGBtoColour(44, 201, 16));
+    // level.texture = assets["snake_texture_2.tex"];
+    this.addModel(ground);
+
+    const rail = ModelCache.newModel("TOWERUNITE_Altitude_10_Rail");
+    rail.position = ground.position;
+    rail.rotation = ground.rotation;
+    rail.scale = ground.scale;
+    rail.calcColour = Model.flatColourLighting(util.RGBtoColour(255, 255, 255), util.RGBtoColour(50, 50, 50));
+    // level.texture = assets["snake_texture_2.tex"];
+    this.addModel(rail);
+
+    const phys = ModelCache.newModel("TOWERUNITE_Altitude_10_Phys");
+    phys.position = ground.position;
+    phys.rotation = ground.rotation;
+    phys.scale = ground.scale;
+    this.addPhysicsModel(phys);
 
     const hole = new HoleEntity(this, new Vector(0, -1.679, 9.991), 0.32);
 
